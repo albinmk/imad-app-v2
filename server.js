f706898b1c95cbc40263d700ec106e248a47b127
albinmk <albinmk3@gmail.com>
2017-02-07 17:04:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    
+    res.send('article -one requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'mapngdi.'));
 });
 
 
