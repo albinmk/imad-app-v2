45dbaaa380b81a7b3c545dcd5e121c8118476cfa
albinmk <albinmk3@gmail.com>
2017-02-26 14:08:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'newtext';
-var img = getElementById('madi');
+var img = document.getElementById('madi');
 
 img.onclick = function() {
     
