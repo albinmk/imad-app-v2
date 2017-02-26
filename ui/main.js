435e470feb6a250185da606829427350e01789dd
albinmk <albinmk3@gmail.com>
2017-02-26 14:29:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'newtext';
 var img = document.getElementById('madi');
 
+var marginLeft = 0;
 function moveRight(){
 
 marginLeft = marginLeft +5;
