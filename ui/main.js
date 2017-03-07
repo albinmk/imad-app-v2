06e1e43ca3133adb39d992f9877a16a354a0996f
albinmk <albinmk3@gmail.com>
2017-03-07 13:54:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,56 @@ img.onclick = function() {
     
  var interval = setInterval(moveRight, 50);
 };*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*index.html former<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        
+        <div class="container"> 
+        <div class="center">
+            
+ <img src="https://i.gadgets360cdn.com/large/moto_g4_red_1486967584004_1487000868858.jpg?output-quality=80&output-format=jpg" class="img-medium"/>
+            
+           <img id="madi" src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div id="main-text" class="center text-big bold">
+            <h4>Hi! I am your webapp and  I have made a change.</h4>
+			
+        </div>
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>*/
