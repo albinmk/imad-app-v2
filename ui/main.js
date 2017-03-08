d2d0f3cbf361eeec9526ab040bf173d1688a26c6
albinmk <albinmk3@gmail.com>
2017-03-08 16:42:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,11 +60,11 @@ button.onclick = function() {
 
 //Submit name
 
-var nameInput = document.getElementById("name");
+var nameInput = document.getElementById('name');
 
 var name = nameInput.value;
 
-var submit = document.getElementById("submit_btn");
+var submit = document.getElementById('submit_btn');
 
 submit.onclick = function () {
     
