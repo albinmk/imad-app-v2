4b3ef05485182c341d350bab2889f282a4505f23
albinmk <albinmk3@gmail.com>
2017-03-08 17:52:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -78,30 +78,29 @@ submit.onclick = function () {
         {
           if(request.status === 200)
           {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
+            //capture a list of names and render it as a list.
+             var names = ['name1','name2','name3','name4'];
+     
+            var list=''; 
+
+          for(var i=0;i<names.length;i++) {
+        
+           list += '<li>' + names[i] + '</li>';
           }
             
-            
+           var ul = document.getElementById('namelist');
+           ul.innerHTML = list; 
         }
         //not yet done
   
     // process the server response
-                           };
+        }
     
     
-    var names = ['name1','name2','name3','name4'];
-     
-    var list=''; 
-
-    for(var i=0;i<names.length;i++){
-        
-        list += '<li>' + names[i] + '</li>';
+    
         
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
     
 };
 
